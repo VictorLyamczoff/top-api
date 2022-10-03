d7d7bd94d2253f72c04b2f3fb1c4f01bc86f2f43
Victor <mister.lyamczoff@yandex.ru>
2022-10-03 22:26:38 +0700

    подключены модели

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,7 +1,22 @@
-export class ReviewModel {
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
+import { prop } from "@typegoose/typegoose";
+
+export interface ReviewModel extends Base {
+}
+
+export class ReviewModel extends TimeStamps {
+  @prop()
   name: string;
+
+  @prop()
   titel: string;
+
+  @prop()
   description: string;
+
+  @prop()
   rating: string;
+
+  @prop()
   createdAt: Date;
 }
