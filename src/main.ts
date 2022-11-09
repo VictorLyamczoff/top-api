92f96ed040181793c28b2fca08001d323830dcdd
Victor <mister.lyamczoff@yandex.ru>
2022-11-09 22:53:54 +0700

    test: test3

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from "./app.module";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
-  console.log("test2");
+  console.log("test3");
 }
 
 bootstrap();
